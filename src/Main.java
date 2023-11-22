4dca661550d468e8abe78403f1432da900477172
sergei <sergei13121390@gmail.com>
2023-11-22 20:23:14 +0300

    Task1_3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача 1");
+        String firstName = "Семён";
+        String middleName = "Иванов";
+        String lastName = "Семёнович";
+        String fullName = middleName + " " + firstName + " " + lastName;
+        System.out.println("Ф. И. О. сотрудника - " + fullName);
+        System.out.println("Задача 2");
+        System.out.println(fullName.toUpperCase());
+        System.out.println("Задача 3");
+        System.out.println("Данные Ф. И. О. сотрудника - " + fullName.replace('ё','е'));
     }
 }
